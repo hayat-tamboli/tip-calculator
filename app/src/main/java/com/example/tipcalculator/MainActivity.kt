d0876166582c53cbf2c22e036034761d38a18566
Hayat Tamboli <hayat.tamboli@gmail.com>
2022-11-27 00:17:16 +0530

    Added functionality for calculating bill

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val buttonCal: Button = binding.buttonCalculateTip
+        val billTextBox: EditText = binding.editTextBill
+        buttonCal.setOnClickListener(){
+            val billAmt: String = (billTextBox.text).toString()
+            val billDbl : Double? = billAmt.toDoubleOrNull()
+            calcBill(billDbl)
+        }
+    }
+    private fun calcBill(billDbl: Double?) {
+        if (billDbl == null) {
+            binding.editTextBill.error = "Enter the bill amount!"
+            DisplayTip(0.0)
+            return
+        }
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId)
+        {
+            R.id.radioBtnAmazing -> 20.0
+            R.id.radioBtnGood -> 15.0
+            R.id.radioBtnOkay -> 10.0
+            else -> 20.0
+        }
+        var final : Double = billDbl + (billDbl*tipPercentage)/100
+        if(binding.switchRoundUpTip.isChecked){
+            final = kotlin.math.ceil(final)
+        }
+        DisplayTip(final)
+    }
+    private fun DisplayTip(amt: Double)
+    {
+        val amtInDollars: String = NumberFormat.getCurrencyInstance().format(amt)
+        binding.textView3.text = amtInDollars
     }
 }
